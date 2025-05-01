69ab78674b5724dc93b65c08e66e1cd1ac1e5c52
Julian <139149286+EinsBackstein@users.noreply.github.com>
2025-05-01 20:11:50 +0200

    example env for sensor

---
+++ src/sensor/src/example.env.h
@@ -0,0 +1,7 @@
+// WLAN-Zugangsdaten – bitte anpassen!
+const char* ssid     = "<SSID>"; // WLAN-SSID
+const char* password = "<Password>"; // WLAN-Passwort
+
+// MQTT-Server-Konfiguration für HiveMQ (öffentlicher Broker)
+const char* mqttServer = "<MQTT_BROKER>"; // MQTT-Server-Adresse
+const int   mqttPort   = 1883; //oder jeder andere Port...| MQTT-Server-Port
